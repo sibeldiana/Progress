9a162363353762adb2a12c793c80a01e2f55b2df
sibeldiana <114567939+sibeldiana@users.noreply.github.com>
2023-05-17 23:03:38 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+const progress = document.getElementsByID('progress');
+const prev = document.getElementsByID('prev');
+const next = document.getElementsByID('next');
+const circles = document.querySelectorAll('.circle');
+let currentActive = 1;
+next.addEventListener('click', () => {
+    currentActive++;
+
+    if (currentActive > circles.length) {
+        currentActive = circles.lenght;
+    }
+
+
+});
+prev.addEventListener('click', () => {
+    currentActive--;
+
+    if (currentActive < 1) {
+        currentActive = 1;
+    }
+
+    update();
+
+});
+function update() {
+    circles.forEach((circle, idx) => {
+        if (idx < currentActive) {
+            circle.classList.add('active');
+        } else {
+            circle.classList.remove('active');
+        }
+    });
+}
